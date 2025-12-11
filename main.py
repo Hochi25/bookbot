9a3515e4523ba20bae0f743dc0b33d9514682bb0
hochi <hochi2503@gmail.com>
2025-12-11 09:18:09 +0100

    finalizing bookbot with variabel paths to books

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from stats import get_num_words
+from stats import get_num_characters
+from stats import character_list
+from stats import get_form
+import sys
+
+
 
 def main():
-    text = get_num_words("books/frankenstein.txt")
-    print(text)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    characters = get_num_characters(sys.argv[1])
+    text = get_num_words(sys.argv[1])
+    sorted = get_form(characters)
+    output = character_list(sorted)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {text} total words")
+    print("--------- Character Count -------")
+    for item in output:
+        char = item["name"]
+        count = item["num"]
+        if char.isalpha() is True:
+           print (f"{char}: {count}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -5,4 +5,31 @@ def get_num_words(file_path):
         i = 0
         for word in words:
             i += 1
-    return f"Found {i} total words"
+    return i
+
+def get_num_characters(file_path):
+    with open(file_path) as c:
+        text = c.read()
+        characters = text.lower()
+        dic = {}
+        for char in characters:
+         
+            if char not in dic:
+                dic[char] = 1
+            else:
+                dic[char] += 1
+        return dic
+    
+def sort_on(items):
+    return items["num"]
+
+def get_form(dictionairy):
+    form = []
+    for k in dictionairy:
+        form.append({"name": k, "num": dictionairy[k]})
+    return form
+
+def character_list(char_list):
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+    
